d2dd014449885931f1937f287ef1332378079055
Kirill Sagach <racedriver791@yandex.ru>
2024-01-31 01:00:35 +0300

    Нарезка компонентов. Страницы

---
+++ src/components/product/product-review.tsx
@@ -0,0 +1,24 @@
+import ProductReviewList from './product-review-list';
+
+function ProductReview(): JSX.Element {
+  return (
+    <section className="review-block">
+      <div className="container">
+        <div className="page-content__headed">
+          <h2 className="title title--h3">Отзывы</h2>
+          <button className="btn" type="button">
+            Оставить свой отзыв
+          </button>
+        </div>
+        <ProductReviewList/>
+        <div className="review-block__buttons">
+          <button className="btn btn--purple" type="button">
+            Показать больше отзывов
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProductReview;
